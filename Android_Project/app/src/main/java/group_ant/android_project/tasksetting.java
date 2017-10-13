248af331d7541a5218592da567c57b9de880181a
xgu64 <xgu64@gatech.edu>
2017-10-13 11:14:44 -0400

    Revert "时间选择后端"
    This reverts commit e0387221f626fec33a1f31acec2270e225635a43.

--- Android_Project/app/src/main/java/group_ant/android_project/tasksetting.java
+++ Android_Project/app/src/main/java/group_ant/android_project/tasksetting.java
@@ -1,16 +1,16 @@
 package group_ant.android_project;
 
+import java.sql.Time;
 import java.util.Calendar;
 
 import android.app.Activity;
-import android.app.DatePickerDialog;
+import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
@@ -19,156 +19,43 @@ import android.widget.TimePicker;
  * Created by guxiaofeng on 10/12/17.
  */
 
-public class tasksetting extends Activity{
-    private EditText showDate = null;
-    private Button pickDate = null;
-    private EditText showTime = null;
-    private Button pickTime = null;
+public class tasksetting extends Activity implements View.OnTouchListener{
 
-    private static final int SHOW_DATAPICK = 0;
-    private static final int DATE_DIALOG_ID = 1;
-    private static final int SHOW_TIMEPICK = 2;
-    private static final int TIME_DIALOG_ID = 3;
-
-    private int mYear;
-    private int mMonth;
-    private int mDay;
-    private int mHour;
-    private int mMinute;
+    private EditText Duedate;
+    private EditText remind_time;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_setting);
 
-        initializeViews();
+        Duedate = (EditText) this.findViewById(R.id.Due_date);
+        remind_time = (EditText) this.findViewById(R.id.Remind_time);
 
-        final Calendar c = Calendar.getInstance();
-        mYear = c.get(Calendar.YEAR);
-        mMonth = c.get(Calendar.MONTH);
-        mDay = c.get(Calendar.DAY_OF_MONTH);
-
-        mHour = c.get(Calendar.HOUR_OF_DAY);
-        mMinute = c.get(Calendar.MINUTE);
-
-        setDateTime();
-        setTimeOfDay();
-    }
-    
-    public void initializeViews(){
-        showDate = (EditText) findViewById(R.id.show_date);
-        pickDate = (Button) findViewById(R.id.pick_date);
-        showTime = (EditText) findViewById(R.id.show_time);
-        pickTime = (Button) findViewById(R.id.pick_time);
-
-        pickDate.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Message msg = new Message();
-                if (pickDate.equals((Button) v)){
-                    msg.what = tasksetting.SHOW_DATAPICK;
-                }
-                tasksetting.this.dateandtimeHandler.sendMessage(msg);
-            }
-        });
-
-        pickTime.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Message msg = new Message();
-                if (pickTime.equals((Button) v)){
-                    msg.what = tasksetting.SHOW_TIMEPICK;
-                }
-                tasksetting.this.dateandtimeHandler.sendMessage(msg);
-            }
-        });
-    }
-
-    private void setDateTime(){
-        final Calendar c = Calendar.getInstance();
-        mYear = c.get(Calendar.YEAR);
-        mMonth = c.get(Calendar.MONTH);
-        mDay = c.get(Calendar.DAY_OF_MONTH);
-
-        updateDateDisplay();
-    }
-
-    private void updateDateDisplay(){
-        showDate.setText(new StringBuilder().append(mYear).append("-")
-                            .append((mMonth+1) < 10 ? "0" + (mMonth+1) : (mMonth+1)).append("-")
-                            .append((mDay < 10) ? "0" + mDay : mDay));
-    }
-
-    private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener(){
-        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
-            mYear = year;
-            mMonth = monthOfYear;
-            mDay = dayOfMonth;
-
-            updateDateDisplay();
-        }
-    };
-
-    private void setTimeOfDay(){
-        final Calendar c = Calendar.getInstance();
-        mHour = c.get(Calendar.HOUR_OF_DAY);
-        mMinute = c.get(Calendar.MINUTE);
-        updateTimeDisplay();
-    }
-
-    private void updateTimeDisplay(){
-        showTime.setText(new StringBuilder().append(mHour).append(":")
-                .append((mMinute < 10) ? "0" + mMinute : mMinute));
-    }
-
-    private TimePickerDialog.OnTimeSetListener mTimeSetListener = new TimePickerDialog.OnTimeSetListener(){
-
-        @Override
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            mHour = hourOfDay;
-            mMinute = minute;
-            updateTimeDisplay();
-        }
-    };
-
-    @Override
-    protected Dialog onCreateDialog(int id){
-        switch (id){
-            case DATE_DIALOG_ID:
-                return new DatePickerDialog(this, mDateSetListener, mYear, mMonth, mDay);
-            case TIME_DIALOG_ID:
-                return new TimePickerDialog(this, mTimeSetListener, mHour, mMinute, true);
-        }
-        return null;
+        Duedate.setOnTouchListener(this);
+        remind_time.setOnTouchListener(this);
     }
 
     @Override
-    protected void onPrepareDialog(int id, Dialog dialog) {
-        switch (id) {
-            case DATE_DIALOG_ID:
-                ((DatePickerDialog) dialog).updateDate(mYear, mMonth, mDay);
-                break;
-            case TIME_DIALOG_ID:
-                ((TimePickerDialog) dialog).updateTime(mHour, mMinute);
-                break;
-        }
-    }
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            View view = View.inflate(this, R.layout.task_setting,null);
+            final DatePicker datePicker = (DatePicker) view.findViewById(R.id.Due_date);
+            final TimePicker timePicker = (android.widget.TimePicker) view.findViewById(R.id.Remind_time);
+            builder.setView(view);
 
-    Handler dateandtimeHandler = new Handler() {
+            Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(System.currentTimeMillis());
+            datePicker.init(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH),null);
 
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case tasksetting.SHOW_DATAPICK:
-                    showDialog(DATE_DIALOG_ID);
-                    break;
-                case tasksetting.SHOW_TIMEPICK:
-                    showDialog(TIME_DIALOG_ID);
-                    break;
+            timePicker.setIs24HourView(true);
+
+            if (v.getId() == R.id.Due_date){
+                final int inType = Duedate.getInputType();
+                Duedate
             }
         }
-
-    };
-
-
+        return false;
+    }
 }
