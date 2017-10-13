ffae90009e976217c805f0efe3dc64b445b0409e
Sec_Zyz <zhangyongzheng@lawn-128-61-40-134.lawn.gatech.edu>
2017-10-12 22:14:11 -0400

    Gg

--- Android_Project/app/src/main/java/group_ant/android_project/MainActivity.java
+++ Android_Project/app/src/main/java/group_ant/android_project/MainActivity.java
@@ -8,10 +8,17 @@ public class MainActivity extends AppCompatActivity {
 
     private Button friend;
     private Button timer;
+    private Button home;
+    private Button tasklist;
+    private Button profile;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.task_layout);
+        friend = (Button)findViewById(R.id.friend);
+        timer = (Button)findViewById(R.id.timer);
+        home = (Button)findViewById(R.id.home);
+        tasklist = (Button)findViewById(R.id.puzzle);
     }
 }
