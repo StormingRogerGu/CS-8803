b357d3973240bdb40d8aa0a4367af4ea4ce9b966
Sec_Zyz <zhangyongzheng@lawn-128-61-40-134.lawn.gatech.edu>
2017-10-12 21:47:33 -0400

    1

--- Android_Project/app/src/main/java/group_ant/android_project/MainActivity.java
+++ Android_Project/app/src/main/java/group_ant/android_project/MainActivity.java
@@ -2,12 +2,13 @@ package group_ant.android_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button friend;
+    private Button timer;
     
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
