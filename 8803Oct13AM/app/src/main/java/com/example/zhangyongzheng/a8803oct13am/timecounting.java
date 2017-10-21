38acdcb6e238a78eb5faaf599ee3994b334084cc
HaiyueYin <31355802+HaiyueYin@users.noreply.github.com>
2017-10-20 22:36:29 -0400

    yhy_new

--- 8803Oct13AM/app/src/main/java/com/example/zhangyongzheng/a8803oct13am/timecounting.java
+++ 8803Oct13AM/app/src/main/java/com/example/zhangyongzheng/a8803oct13am/timecounting.java
@@ -34,11 +34,17 @@ public class timecounting extends Activity{
     long current_seconds;
     private boolean button_resume = true;
 
+    private int notifyId = 100;
+    private NotificationManager mNotificationManager;
+    public NotificationCompat.Builder mBuilder;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.time_count);
         mTvShow = (TextView) findViewById(R.id.counting_time);
         pause_resume = (Button) findViewById(R.id.pause_resume_button);
+
+        initNotify();
 
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
@@ -52,8 +58,40 @@ public class timecounting extends Activity{
         myCount = new Mycount(seconds, 1000);
         myCount.start();
         setPause_Resume();
+    }
+
+    private void initNotify() {
+        mNotificationManager = (NotificationManager) getSystemService((NOTIFICATION_SERVICE));
+        mBuilder = new NotificationCompat.Builder(getApplicationContext());
+        mBuilder.setContentTitle("3")
+                //.setContentText("You win a new puzzle!")
+                .setContentIntent(getDefaultIntent(Notification.FLAG_AUTO_CANCEL))
+                .setTicker("5")
+                .setWhen(System.currentTimeMillis())
+                .setPriority(Notification.PRIORITY_DEFAULT)
+                .setOngoing(false)
+                .setSmallIcon(R.drawable.puzzle_icon_highlight);
 
     }
+
+    public PendingIntent getDefaultIntent(int flags) {
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1 ,new Intent(), flags);
+        return pendingIntent;
+    }
+
+    public void showIntentActivityNotify(String who) {
+        mBuilder.setAutoCancel(true)
+                .setContentTitle(who)
+                .setWhen(System.currentTimeMillis())
+                .setTicker("6");
+
+        Intent resultIntent = new Intent(this, timecounting.class);
+        resultIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setContentIntent(pendingIntent);
+        mNotificationManager.notify(notifyId, mBuilder.build());
+    }
+
 
     class Mycount extends CountDownTimer{
         long timetofinfish;
@@ -65,6 +103,9 @@ public class timecounting extends Activity{
         @Override
         public void onFinish() {
             mTvShow.setText("You have successfully finished your study time!");
+
+            mBuilder.setDefaults(Notification.DEFAULT_VIBRATE);
+            showIntentActivityNotify("Congratulations! New Puzzle!");
 
         //TODO: AFITER FINISH THE TIME MODE, SEND MESSAGE TO PUZZLE GALLERY AND
 //            Intent jumptogallery = new Intent();
