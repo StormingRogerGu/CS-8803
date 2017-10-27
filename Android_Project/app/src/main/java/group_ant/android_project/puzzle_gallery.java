80add42204a86b575d659059ee7c4224c407ca10
xgu64 <xgu64@gatech.edu>
2017-10-27 15:39:57 -0400

    shanchu

--- Android_Project/app/src/main/java/group_ant/android_project/puzzle_gallery.java
+++
@@ -1,8 +0,0 @@
-package group_ant.android_project;
-
-/**
- * Created by Yu on 10/12/2017.
- */
-
-public class puzzle_gallery {
-}
--- Android_Project/app/src/main/java/group_ant/android_project/time_mode.java
+++
@@ -1,8 +0,0 @@
-package group_ant.android_project;
-
-/**
- * Created by haiyueyin on 10/13/17.
- */
-
-public class time_mode {
-}
